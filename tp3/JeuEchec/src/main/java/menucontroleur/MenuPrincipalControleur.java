374c1b19438f45b08fbd62315e07905a85166f63
JM Poirier <poirier.jmp@gmail.com>
2017-11-24 12:06:07 -0500

    TP3 - Header License

--- tp3/JeuEchec/src/main/java/menucontroleur/MenuPrincipalControleur.java
+++ tp3/JeuEchec/src/main/java/menucontroleur/MenuPrincipalControleur.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2017 Jean-Michel Poirier et Audrey Eugene.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package menucontroleur;
 
 import java.io.IOException;
@@ -10,6 +25,9 @@ import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.AnchorPane;
 
+/**
+ * FXML Controller class
+ */
 public class MenuPrincipalControleur implements Initializable {
 
     @FXML
