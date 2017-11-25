59792c1745698c4c65bc30467228c71902721938
JM Poirier <poirier.jmp@gmail.com>
2017-11-25 15:07:54 -0500

    TP3 - Logique pour la classe Table

--- tp3/JeuEchec/src/main/java/jeu/Table.java
+++ tp3/JeuEchec/src/main/java/jeu/Table.java
@@ -15,18 +15,29 @@
  */
 package jeu;
 
+import java.util.ArrayList;
+import piece.Cavalier;
+import piece.Dame;
+import piece.Fou;
 import piece.Piece;
+import piece.Pion;
+import piece.Roi;
+import piece.Tour;
 
 /**
  *
  * @author jmppr
  */
 public class Table {
+    final String COULEUR1 = "Noir";
+    final String COULEUR2 = "Blanc";
+    final int NB_COL = 8;
+    final int NB_ROW = 8;
 
     Piece tablePieces[][];
 
-    public Table(Piece tablePices[][]) {
-        this.tablePieces = new Piece[8][8];
+    public Table() {
+        this.tablePieces = new Piece[NB_COL][NB_ROW];
     }
 
     public Piece getPiece(int x, int y) {
@@ -35,6 +46,55 @@ public class Table {
 
     public void setPiece(int x, int y, Piece piece) {
         tablePieces[x][y] = piece;
+    }
+
+    private ArrayList<Piece> listeTousLesPieces() {
+        ArrayList<Piece> piecesListe = new ArrayList<>();
+
+        piecesListe.addAll(listePiecesCouleur(COULEUR1));
+        piecesListe.addAll(listePiecesCouleur(COULEUR2));
+        return piecesListe;
+    }
+
+    private ArrayList<Piece> listePiecesCouleur(String couleur) {
+        ArrayList<Piece> piecesListe = new ArrayList<>();
+        int col = (COULEUR1.equals(couleur)) ? 0 : 7;
+        int colPion = (COULEUR2.equals(couleur)) ? 1 : 6;
+
+        piecesListe.add(new Tour(couleur, 0, col));
+        piecesListe.add(new Cavalier(couleur, 1, col));
+        piecesListe.add(new Fou(couleur, 2, col));
+        piecesListe.add(new Dame(couleur, 3, col));
+        piecesListe.add(new Roi(couleur, 4, col));
+        piecesListe.add(new Fou(couleur, 5, col));
+        piecesListe.add(new Cavalier(couleur, 6, col));
+        piecesListe.add(new Tour(couleur, 7, col));
+        piecesListe.addAll(listePiecesPion(colPion));
+        return piecesListe;
+    }
+
+    private ArrayList<Piece> listePiecesPion(int col) {
+        ArrayList<Piece> pionsListe = new ArrayList<>();
+        for (int i = 0; i < NB_COL; i++) {
+            pionsListe.add(new Pion((col == 1) ? COULEUR1 : COULEUR2, i, col));
+        }
+        return pionsListe;
+    }
+
+    public void initialiserNouvelleTable() {
+        for (int i = 0; i < NB_COL; i++) {
+            for (int j = 0; j < NB_ROW; j++) {
+                for (int k = 0; k < listeTousLesPieces().size(); k++) {
+                    if (i == listeTousLesPieces().get(k).getPosX() && j == listeTousLesPieces().get(k).getPosY()) {
+                        tablePieces[i][j] = listeTousLesPieces().get(k);
+                    }
+                }
+            }
+        }
+    }
+
+    public void initialiserTableSauvegarder() {
+        //TODO
     }
 
 }
--- tp3/JeuEchec/src/main/java/piece/Piece.java
+++ tp3/JeuEchec/src/main/java/piece/Piece.java
@@ -39,7 +39,7 @@ public abstract class Piece {
         return posY;
     }
 
-    public String couleur() {
+    public String getCouleur() {
         return couleur;
     }
 
